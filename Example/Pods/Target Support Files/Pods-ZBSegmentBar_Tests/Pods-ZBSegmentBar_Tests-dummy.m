68f3d68a781fb9c7e061b08940d839b68e965eda
zzb <835066041@qq.com>
2018-01-14 15:44:36 +0800

    base project

---
+++ Example/Pods/Target Support Files/Pods-ZBSegmentBar_Tests/Pods-ZBSegmentBar_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZBSegmentBar_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_ZBSegmentBar_Tests
+@end
---
+++ Example/Pods/Target Support Files/ZBSegmentBar/ZBSegmentBar-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ZBSegmentBar : NSObject
+@end
+@implementation PodsDummy_ZBSegmentBar
+@end
