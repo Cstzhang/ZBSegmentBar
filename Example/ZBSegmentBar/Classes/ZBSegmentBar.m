1b00c1675f2413cb8585bf93f4d3def234e7e763
zzb <835066041@qq.com>
2018-01-14 16:09:52 +0800

    title 滑动动画

--- Example/ZBSegmentBar/Classes/ZBSegmentBar.m
+++ Example/ZBSegmentBar/Classes/ZBSegmentBar.m
@@ -12,11 +12,16 @@
 /// space min width
 #define kMinMargin 30
 
-@interface ZBSegmentBar ()
-// main content view
+@interface ZBSegmentBar (){
+    /// the last clicked title button
+    UIButton *_lastButton;
+}
+/// main content view
 @property (nonatomic, weak) UIScrollView *contentView;
 /// title button
 @property (nonatomic, strong) NSMutableArray <UIButton *>*itemBtns;
+/// indicator View
+@property (nonatomic, weak) UIView *indicatorView;
 
 @end
 
@@ -33,7 +38,6 @@
     return segmentBar;
 }
 
-
 /**
  itemBtns lazy func
 
@@ -45,7 +49,21 @@
     }
     return _itemBtns;
 }
-
+/**
+  indicatorView (line) lazy func
+ 
+ @return indicatorView (line)
+ */
+- (UIView *)indicatorView {
+    if (!_indicatorView) {
+        CGFloat indicatorH = 2;
+        UIView *indicatorView = [[UIView alloc] initWithFrame:CGRectMake(0, self.height - indicatorH, 0, indicatorH)];
+        indicatorView.backgroundColor = [UIColor redColor];
+        [self.contentView addSubview:indicatorView];
+        _indicatorView = indicatorView;
+    }
+    return _indicatorView;
+}
 
 /**
 set title items
@@ -60,6 +78,9 @@ set title items
     // add item buttons in content view
     for (NSString *item in items) {
         UIButton *btn = [[UIButton alloc] init];
+        [btn addTarget:self action:@selector(btnClick:)    forControlEvents:UIControlEventTouchDown];
+        [btn setTitleColor:[UIColor lightGrayColor] forState:UIControlStateNormal];
+        [btn setTitleColor:[UIColor greenColor] forState:UIControlStateSelected];
         [btn setTitle:item forState:UIControlStateNormal];
         [self.contentView addSubview:btn];
         [self.itemBtns addObject:btn];
@@ -70,6 +91,35 @@ set title items
     
 }
 
+/**
+ title button click
+
+ @param btn button clicked
+ */
+- (void)btnClick: (UIButton *)btn {
+    // 1. set last clicked button
+    _lastButton.selected = NO;
+    btn.selected = YES;
+    _lastButton = btn;
+    
+    // 2. add animation
+    [UIView animateWithDuration:0.1 animations:^{
+        self.indicatorView.width = btn.width;
+        self.indicatorView.centerX = btn.centerX;
+    }];
+    
+    // 3. indicatorView (line)  scroll to button
+    CGFloat scrollX = btn.centerX - self.contentView.width * 0.5;
+    // need not scroll
+    if (scrollX < 0) {
+        scrollX = 0;
+    }
+    // set  max scroll X
+    if (scrollX > self.contentView.contentSize.width - self.contentView.width) {
+        scrollX = self.contentView.contentSize.width - self.contentView.width;
+    }
+    [self.contentView setContentOffset:CGPointMake(scrollX, 0) animated:YES];
+}
 
 /**
  update the layout
--- Example/ZBSegmentBar/ZBSegmentBarViewController.m
+++ Example/ZBSegmentBar/ZBSegmentBarViewController.m
@@ -18,12 +18,13 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    CGRect frame = CGRectMake(0, 0, self.view.width, 35);
+    CGRect frame = CGRectMake(0, 20, self.view.width, 35);
     ZBSegmentBar *segmentBar = [ZBSegmentBar segmentBarWithFrame:frame];
     segmentBar.backgroundColor = [UIColor redColor];
     [self.view addSubview:segmentBar];
-    segmentBar.items = @[@"专辑", @"声音", @"下载中", @"已完成",@"已删除"];
-//    segmentBar.items = @[@"专辑", @"声音", @"下载中"];
+   // segmentBar.items = @[@"专辑", @"声音", @"下载中", @"已完成",@"已删除"];
+    segmentBar.items = @[@"1专辑", @"1声音", @"1下载中", @"1已完成",@"1已删除",@"2专辑", @"2声音", @"2下载中", @"2已完成",@"2已删除",@"3专辑", @"3声音", @"3下载中", @"3已完成",@"3已删除"];
+   //    segmentBar.items = @[@"专辑", @"声音", @"下载中"];
 }
 
 - (void)didReceiveMemoryWarning
